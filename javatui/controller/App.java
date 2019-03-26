f9f6327cae0f23233c52477cc6d5a768d9930825
code-witch <code-witch@users.noreply.github.com>
2019-03-26 15:54:37 -0600

    Add Comments

--- javatui/controller/App.java
+++ javatui/controller/App.java
@@ -1,28 +1,35 @@
 package controller;
 
+import events.*;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public final class App extends Application {
-    public final VBox root = new VBox();
-    public final Scene scene = new Scene(root);
+    public static final HBox root = new HBox();
+    public static final Scene scene = new Scene(root);
+
 
     @Override
     public void init() {
-
+        // Put more init stuff here if need be.
+        System.out.println("Initialization Completed");
     }
 
 
     @Override
     public void start(Stage stage) {
-
-
+        // There's got to be a cleaner way..
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, new WidgetAdd());
 
         stage.setTitle("JavaTUI");
         stage.setScene(scene);
         stage.show();
     }
+
+
 
 }
